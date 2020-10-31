ae1da4251b8f19d0c1d2c1b19edd5afdf8ead859
moatefdev <m.atef317@gmail.com>
2020-10-31 11:46:34 +0200

    Added some JavaScript code

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,13 @@
+var portfolioLinks = document.querySelectorAll('.techniques li');
+
+for(var i = 0; i < portfolioLinks.length; i++)
+{
+  portfolioLinks[i].addEventListener("click", function () {
+    alert("These links are under construction (not coded yet).");
+  })
+}
+
+
 // document.ready(function (){
 
 //   'use strict';
